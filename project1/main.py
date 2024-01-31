a9323020deed90ba5c4c09c49817789c0b1eaaac
Wiktor Szwajka <wiktorszwajka@gmail.com>
2024-01-31 15:28:55 +0100

    Rock paper scissors - User vs Computer

--- project1/main.py
+++ project1/main.py
@@ -1,73 +1,71 @@
-# papier kamien nozyce
-# user i komputer gramy do 3
-# wypisac zwyciezce i wynik 
-# jesli wpisze niepoprawny input to ogarnąć
+# niech graja ze soba dwa kompy
+# Rózne strategie: 1: Losowo , Druga : pierwsza odp losowo potem caly czas to samo 3: Pierwsza losowo a kada nastepna inna odp porzedniej
+
 import random
 
 
+def who_wins(answer1, answer2):
+    if answer1 == 's' and answer2 == 'p':
+        return 1
 
-# def main():
-        
-answers = ['s', 'p', 'r']
-user_points = 0
-computer_points = 0
+    if answer1 =='p' and answer2 == 's':
+        return 2
+
+    if answer1 =='s' and answer2 == 'r':
+        return 2
+
+    if answer1 =='r' and answer2 == 's':
+        return 1
+
+    if answer1 =='r' and answer2 == 'p':
+        return 2
+
+    if answer1 =='p' and answer2 == 'r':
+        return 1
+
+    if answer1 =='r' and answer2 == 'r':
+        return 0
+
+    if answer1 =='p' and answer2 == 'p':
+        return 0
+
+    if answer1 =='s' and answer2 == 's':
+        return 0
+
+    return -1 
 
 
-while user_points < 3 or computer_points < 3:
-    print (f'Users points: {user_points} \nComputer points: {computer_points}')
-    user = input("Type s,p,r for 'scissor' 'paper' 'rock'")
-    computer = random.choice(answers)
-    print(f'Computer choice is {computer}')
-    if user == 's' and computer == 'p':
-        print('Users wins!')
-        user_points += 1
-    elif user =='p' and computer == 's':
-        print('Computer Wins!')
-        computer_points += 1    
-    elif user =='s' and computer == 'r':
-        print('Computer Wins!')
-        computer_points += 1
-    elif user =='r' and computer == 's':
-        print('User Wins!')
-        user_points += 1
-    elif user =='r' and computer == 'p':
-        print('Computer Wins!')
-        computer_points += 1
-    elif user =='p' and computer == 'r':
-        print('User Wins!')
-        user_points += 1
-    elif user =='r' and computer == 'r':
-        print('Draw!')
-    elif user =='p' and computer == 'p':
-        print('Draw!')
-    elif user =='s' and computer == 's':
-        print('Draw!')
+def main():
+
+    answers = ['s', 'p', 'r']
+    user_points = 0
+    computer_points = 0
+
+    while user_points != 3 and computer_points != 3:
+        print (f'Users points: {user_points} \nComputer points: {computer_points}')
+        user = input("Type s,p,r for 'scissor' 'paper' 'rock'")
+        computer = random.choice(answers)
+        print(f'Computer choice is {computer}')
+        winner =  who_wins(user,computer)
+        if winner == 1:
+            user_points += 1
+        elif winner == 2:
+            computer_points += 1
+        elif winner == 0:
+            print('Draw!')
+        else:
+            print('Please insert r, p, c')
+
+    if user_points > computer_points:
+        print(f'User Wins the game! {user_points}:{computer_points}')
     else:
-        print('Please insert s, r ,c')
-
-    if user_points == 3 and computer_points < 3:
-        print (f'Users wins whole match with {user_points} points  \nComputer points: {computer_points}')
-        break
-    elif user_points < 3 and computer_points == 3:           
-        print (f'Computer wins whole match with {computer_points} points  \nUser points: {user_points}')
-        break
-
-# return 
-    
-
-
-        
-
-
-
-            
+        print(f'Computer Wins the game! {computer_points}:{user_points}')
 
 
 
 
-
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
 
 
 
